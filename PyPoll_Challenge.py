4343cc753be2636a0836ac85a5e3c7e6eaeca994
hamedhi66 <hamed.hakimelahi@gmail.com>
2019-10-19 16:51:32 -0700

    Adding PyPoll_Challenge.py file.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -1,4 +1,3 @@
-# Add our dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
@@ -6,8 +5,51 @@ file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Initialize a total vote counter.
-total_votes = 0
+# Counties Options
+county_options = []
+county_dic= {}
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    total_votes_county=0
+    for row in file_reader:
+        total_votes_county +=1
+        county_name = row[1]
+
+        if county_name not in county_options:
+            county_options.append(county_name)
+            county_dic[county_name]=0
+        county_dic[county_name] +=1
+print("Election Results")
+print(f"-------------------------")
+print("Total votes cast: ",total_votes_county)
+print(f"-------------------------\n")
+
+#4. Create an empty string that will hold the 
+#county name that had the largest turnout.
+winning_vote=0
+for  counties in county_dic:
+    #counties:key
+    county_votes= county_dic.get(counties)
+    #print(county_votes)
+    vote_percentage= float(county_votes)/float(total_votes_county)*100
+
+    print(f"{counties}: {vote_percentage:.1f}% ({county_votes:,})")
+    
+    if county_votes > winning_vote:
+        winning_vote= county_votes
+        winning_county=counties
+#print(winning_vote)
+print('')
+print(f"-------------------------")
+print("Largest County Turnout:", winning_county)
+print(f"-------------------------")
+
 
 # Candidate Options
 candidate_options = []
@@ -17,6 +59,7 @@ candidate_votes={}
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+total_votes_candidate=0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -28,7 +71,7 @@ with open(file_to_load) as election_data:
     # Print each row in the CSV file.
     for row in file_reader:
         # Add to the total vote count.
-        total_votes += 1
+        total_votes_candidate += 1
 
         # Print the candidate name from each row.
         candidate_name = row[2]
@@ -45,7 +88,7 @@ for candidate in candidate_votes:
     # Retrieve vote count of a candidate.
     votes = candidate_votes[candidate]
     # Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
+    vote_percentage = float(votes) / float(total_votes_candidate) * 100
 
     #  To do: print out each candidate's name, vote count, and percentage of
     # votes to the terminal.
@@ -62,7 +105,7 @@ for candidate in candidate_votes:
 
 # To do: print out each candidate's name, vote count, and percentage of
 # votes to the terminal.
-    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})")
 
 winning_candidate_summary = (
     f"-------------------------\n"
